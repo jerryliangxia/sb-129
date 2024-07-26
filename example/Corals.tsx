27d7d21d3a870022924b050d0655c92319ea3bfa
Jerry Xia <jerrylxia@gmail.com>
2024-07-26 02:47:40 -0400

    Simplified colliders for meshes, reduce spawning of big rocks

--- example/Corals.tsx
+++ example/Corals.tsx
@@ -1,155 +1,20 @@
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
-import { RigidBody } from "@react-three/rapier";
+import React from "react";
 import * as THREE from "three";
-
-const OBJECT_TYPES = [
-  {
-    geometry: "Cylinder018",
-    materials: [
-      "DarkCoralMaterial",
-      "Black",
-      "Rock",
-      "BlackOutline",
-      "UrchinBlack",
-      "SmallCoralMaterial",
-      "SmallCoralOutline",
-      "YellowSandOutline",
-    ],
-  },
-  {
-    geometry: "Plane104",
-    materials: [
-      "BlueSandOutline",
-      "YellowSandOutline",
-      "DarkCoralMaterial",
-      "Black",
-      "Rock",
-      "BlackOutline",
-      "SmallCoralMaterial",
-      "SmallCoralOutline",
-      "UrchinBlack",
-    ],
-  },
-  {
-    geometry: "Plane105",
-    materials: [
-      "BlueSandOutline",
-      "GreenSandOutline.001",
-      "Rock",
-      "BlackOutline",
-      "SmallCoralMaterial",
-      "SmallCoralOutline",
-      "UrchinBlack",
-      "Kelp",
-      "KelpOutline",
-    ],
-  },
-  {
-    geometry: "Plane106",
-    materials: [
-      "BlueSandOutline",
-      "TallCoralOutline",
-      "Kelp",
-      "KelpOutline",
-      "Rock",
-      "BlackOutline",
-      "SmallCoralMaterial",
-      "SmallCoralOutline",
-      "UrchinBlack",
-      "OrangeCoral",
-      "Black",
-    ],
-  },
-  { geometry: "Plane010", materials: ["Kelp", "KelpOutline"] },
-  { geometry: "Plane015", materials: ["Kelp", "KelpOutline"] },
-  { geometry: "Plane019", materials: ["Kelp", "KelpOutline"] },
-  {
-    geometry: "Plane051",
-    materials: [
-      "SmallCoralMaterial",
-      "SandOutline",
-      "BigRock",
-      "PurpleOutline",
-      "Kelp",
-      "KelpOutline",
-      "KelpOrange",
-      "KelpOrangeOutline",
-      "SmallCoralOutline",
-      "OrangeCoral",
-      "Black",
-      "UrchinBlack",
-      "YellowSandOutline",
-      "TallCoralOutline",
-    ],
-  },
-  {
-    geometry: "Plane061",
-    materials: [
-      "SmallCoralMaterial",
-      "SandOutline",
-      "BigRock",
-      "PurpleOutline",
-      "Kelp",
-      "KelpOutline",
-      "SmallCoralOutline",
-      "Black",
-      "DarkCoralMaterial",
-      "YellowSandOutline",
-      "KelpOrangeOutline",
-      "UrchinBlack",
-    ],
-  },
-  {
-    geometry: "Cube009",
-    materials: [
-      "BigRock",
-      "PurpleOutline",
-      "RedCoral",
-      "Black",
-      "Kelp",
-      "KelpOutline",
-      "SmallCoralMaterial",
-      "SmallCoralOutline",
-      "KelpOrange",
-      "KelpOrangeOutline",
-      "SandOutline",
-      "YellowSandOutline",
-      "UrchinBlack",
-    ],
-  },
-  {
-    geometry: "Cylinder004",
-    materials: ["TallCoralMaterial", "TallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder005",
-    materials: ["TallCoralMaterial", "TallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder006",
-    materials: ["TallCoralMaterial", "TallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder007",
-    materials: ["TallCoralMaterial", "TallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder008",
-    materials: ["TallCoralMaterial", "TallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder010",
-    materials: ["SmallCoralMaterial", "SmallCoralOutline"],
-  },
-  {
-    geometry: "Cylinder011",
-    materials: ["SmallCoralMaterial", "SmallCoralOutline"],
-  },
-];
+import { Canvas } from "@react-three/fiber";
+import {
+  Rock,
+  Rock001,
+  Rock002,
+  BigRock,
+  BigRock001,
+  BigRock002,
+  TallCoral,
+  SmallCoral,
+} from "./MeshComponents";
+import { useGLTF } from "@react-three/drei";
 
 const RADIUS = 300;
-const OBJECT_COUNT = 50; // Adjust as needed
+const OBJECT_COUNT = 150;
 
 function getRandomPosition(existingPositions, radius) {
   let position;
@@ -162,54 +27,58 @@ function getRandomPosition(existingPositions, radius) {
     );
     isOverlapping = existingPositions.some(
       (pos) => pos.distanceTo(position) < 10
-    ); // Adjust distance as needed
+    );
   } while (isOverlapping);
   return position;
 }
 
-export default function EnvironmentSpawner(props) {
-  const { nodes, materials } = useGLTF("/environment_grouped.glb");
+export default function EnvironmentSpawner() {
   const positions = [];
 
   for (let i = 0; i < OBJECT_COUNT; i++) {
     positions.push(getRandomPosition(positions, RADIUS));
   }
 
+  // Weighted array for components
+  const components = [
+    Rock,
+    Rock,
+    Rock001,
+    Rock001,
+    Rock002,
+    Rock002,
+    BigRock,
+    BigRock001,
+    BigRock002,
+    TallCoral,
+    TallCoral,
+    TallCoral,
+    TallCoral,
+    SmallCoral,
+    SmallCoral,
+    SmallCoral,
+    SmallCoral,
+  ];
+
   return (
-    <group {...props} dispose={null}>
+    <>
       {positions.map((position, index) => {
-        const objectType =
-          OBJECT_TYPES[Math.floor(Math.random() * OBJECT_TYPES.length)];
-        const scale = Math.random() * 0.5 + 0.5; // Random scale between 0.5 and 1.0
-        const rotation = new THREE.Euler(0, Math.random() * Math.PI * 2, 0); // Random rotation
+        // Adjust the Y position and apply a random Y rotation
+        const adjustedPosition = [position.x, position.y - 1, position.z];
+        const randomYRotation = Math.random() * Math.PI * 2; // Random rotation between 0 and 2π
+
+        const Component =
+          components[Math.floor(Math.random() * components.length)];
         return (
-          <RigidBody
+          <Component
             key={index}
-            // colliders="hull"
-            colliders="trimesh"
-            type="fixed"
-            position={[position.x, position.y - 1, position.z]}
-            rotation={rotation}
-          >
-            <group scale={scale}>
-              {objectType.materials.map((material, i) => (
-                <mesh
-                  key={i}
-                  castShadow
-                  receiveShadow
-                  geometry={
-                    nodes[`${objectType.geometry}_${i}`]?.geometry ||
-                    nodes[objectType.geometry].geometry
-                  }
-                  material={materials[material]}
-                />
-              ))}
-            </group>
-          </RigidBody>
+            position={adjustedPosition}
+            rotation={[0, randomYRotation, 0]}
+          />
         );
       })}
-    </group>
+    </>
   );
 }
 
-useGLTF.preload("/environment_grouped.glb");
+useGLTF.preload("/environment_colliders.glb");
---
+++ example/MeshComponents.tsx
@@ -0,0 +1,518 @@
+import React from "react";
+import { RigidBody } from "@react-three/rapier";
+import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+
+export function Rock({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+
+  return (
+    <group position={position} rotation={rotation} name="Rock">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderRock.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group name="Rock">
+        <mesh
+          name="Plane105"
+          geometry={nodes.Plane105.geometry}
+          material={materials.BlueSandOutline}
+        />
+        <mesh
+          name="Plane105_1"
+          geometry={nodes.Plane105_1.geometry}
+          material={materials["GreenSandOutline.001"]}
+        />
+        <mesh
+          name="Plane105_2"
+          geometry={nodes.Plane105_2.geometry}
+          material={materials.Rock}
+        />
+        <mesh
+          name="Plane105_3"
+          geometry={nodes.Plane105_3.geometry}
+          material={materials.BlackOutline}
+        />
+        <mesh
+          name="Plane105_4"
+          geometry={nodes.Plane105_4.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Plane105_5"
+          geometry={nodes.Plane105_5.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Plane105_6"
+          geometry={nodes.Plane105_6.geometry}
+          material={materials.UrchinBlack}
+        />
+        <mesh
+          name="Plane105_7"
+          geometry={nodes.Plane105_7.geometry}
+          material={materials.Kelp}
+        />
+        <mesh
+          name="Plane105_8"
+          geometry={nodes.Plane105_8.geometry}
+          material={materials.KelpOutline}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function Rock001({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+
+  return (
+    <group position={position} rotation={rotation} name="Rock001">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderRock001.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group name="Rock001">
+        <mesh
+          name="Cylinder018"
+          geometry={nodes.Cylinder018.geometry}
+          material={materials.DarkCoralMaterial}
+        />
+        <mesh
+          name="Cylinder018_1"
+          geometry={nodes.Cylinder018_1.geometry}
+          material={materials.Black}
+        />
+        <mesh
+          name="Cylinder018_2"
+          geometry={nodes.Cylinder018_2.geometry}
+          material={materials.Rock}
+        />
+        <mesh
+          name="Cylinder018_3"
+          geometry={nodes.Cylinder018_3.geometry}
+          material={materials.BlackOutline}
+        />
+        <mesh
+          name="Cylinder018_4"
+          geometry={nodes.Cylinder018_4.geometry}
+          material={materials.UrchinBlack}
+        />
+        <mesh
+          name="Cylinder018_5"
+          geometry={nodes.Cylinder018_5.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Cylinder018_6"
+          geometry={nodes.Cylinder018_6.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Cylinder018_7"
+          geometry={nodes.Cylinder018_7.geometry}
+          material={materials.YellowSandOutline}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function Rock002({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+
+  return (
+    <group position={position} rotation={rotation} name="Rock001">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderRock002.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group name="Rock002">
+        <mesh
+          name="Plane106"
+          geometry={nodes.Plane106.geometry}
+          material={materials.BlueSandOutline}
+        />
+        <mesh
+          name="Plane106_1"
+          geometry={nodes.Plane106_1.geometry}
+          material={materials.TallCoralOutline}
+        />
+        <mesh
+          name="Plane106_2"
+          geometry={nodes.Plane106_2.geometry}
+          material={materials.Kelp}
+        />
+        <mesh
+          name="Plane106_3"
+          geometry={nodes.Plane106_3.geometry}
+          material={materials.KelpOutline}
+        />
+        <mesh
+          name="Plane106_4"
+          geometry={nodes.Plane106_4.geometry}
+          material={materials.Rock}
+        />
+        <mesh
+          name="Plane106_5"
+          geometry={nodes.Plane106_5.geometry}
+          material={materials.BlackOutline}
+        />
+        <mesh
+          name="Plane106_6"
+          geometry={nodes.Plane106_6.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Plane106_7"
+          geometry={nodes.Plane106_7.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Plane106_8"
+          geometry={nodes.Plane106_8.geometry}
+          material={materials.UrchinBlack}
+        />
+        <mesh
+          name="Plane106_9"
+          geometry={nodes.Plane106_9.geometry}
+          material={materials.OrangeCoral}
+        />
+        <mesh
+          name="Plane106_10"
+          geometry={nodes.Plane106_10.geometry}
+          material={materials.Black}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function BigRock({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+  return (
+    <group position={position} rotation={rotation} name="BigRock">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderBigRock.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group name="BigRock">
+        <mesh
+          name="Plane061"
+          geometry={nodes.Plane061.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Plane061_1"
+          geometry={nodes.Plane061_1.geometry}
+          material={materials.SandOutline}
+        />
+        <mesh
+          name="Plane061_2"
+          geometry={nodes.Plane061_2.geometry}
+          material={materials.BigRock}
+        />
+        <mesh
+          name="Plane061_3"
+          geometry={nodes.Plane061_3.geometry}
+          material={materials.PurpleOutline}
+        />
+        <mesh
+          name="Plane061_4"
+          geometry={nodes.Plane061_4.geometry}
+          material={materials.Kelp}
+        />
+        <mesh
+          name="Plane061_5"
+          geometry={nodes.Plane061_5.geometry}
+          material={materials.KelpOutline}
+        />
+        <mesh
+          name="Plane061_6"
+          geometry={nodes.Plane061_6.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Plane061_7"
+          geometry={nodes.Plane061_7.geometry}
+          material={materials.Black}
+        />
+        <mesh
+          name="Plane061_8"
+          geometry={nodes.Plane061_8.geometry}
+          material={materials.DarkCoralMaterial}
+        />
+        <mesh
+          name="Plane061_9"
+          geometry={nodes.Plane061_9.geometry}
+          material={materials.YellowSandOutline}
+        />
+        <mesh
+          name="Plane061_10"
+          geometry={nodes.Plane061_10.geometry}
+          material={materials.KelpOrangeOutline}
+        />
+        <mesh
+          name="Plane061_11"
+          geometry={nodes.Plane061_11.geometry}
+          material={materials.UrchinBlack}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function BigRock001({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+  return (
+    <group position={position} rotation={rotation} name="BigRock001">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderBigRock001.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group>
+        <mesh
+          name="Plane051"
+          castShadow
+          geometry={nodes.Plane051.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Plane051_1"
+          castShadow
+          geometry={nodes.Plane051_1.geometry}
+          material={materials.SandOutline}
+        />
+        <mesh
+          name="Plane051_2"
+          castShadow
+          geometry={nodes.Plane051_2.geometry}
+          material={materials.BigRock}
+        />
+        <mesh
+          name="Plane051_3"
+          castShadow
+          geometry={nodes.Plane051_3.geometry}
+          material={materials.PurpleOutline}
+        />
+        <mesh
+          name="Plane051_4"
+          castShadow
+          geometry={nodes.Plane051_4.geometry}
+          material={materials.Kelp}
+        />
+        <mesh
+          name="Plane051_5"
+          castShadow
+          geometry={nodes.Plane051_5.geometry}
+          material={materials.KelpOutline}
+        />
+        <mesh
+          name="Plane051_6"
+          castShadow
+          geometry={nodes.Plane051_6.geometry}
+          material={materials.KelpOrange}
+        />
+        <mesh
+          name="Plane051_7"
+          castShadow
+          geometry={nodes.Plane051_7.geometry}
+          material={materials.KelpOrangeOutline}
+        />
+        <mesh
+          name="Plane051_8"
+          castShadow
+          geometry={nodes.Plane051_8.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Plane051_9"
+          castShadow
+          geometry={nodes.Plane051_9.geometry}
+          material={materials.OrangeCoral}
+        />
+        <mesh
+          name="Plane051_10"
+          castShadow
+          geometry={nodes.Plane051_10.geometry}
+          material={materials.Black}
+        />
+        <mesh
+          name="Plane051_11"
+          castShadow
+          geometry={nodes.Plane051_11.geometry}
+          material={materials.UrchinBlack}
+        />
+        <mesh
+          name="Plane051_12"
+          castShadow
+          geometry={nodes.Plane051_12.geometry}
+          material={materials.YellowSandOutline}
+        />
+        <mesh
+          name="Plane051_13"
+          castShadow
+          geometry={nodes.Plane051_13.geometry}
+          material={materials.TallCoralOutline}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function BigRock002({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+  return (
+    <group position={position} rotation={rotation} name="BigRock002">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderBigRock002.geometry}>
+          <meshBasicMaterial opacity={0} transparent side={THREE.BackSide} />
+        </mesh>
+      </RigidBody>
+      <group name="BigRock002">
+        <mesh
+          name="Cube009"
+          geometry={nodes.Cube009.geometry}
+          material={materials.BigRock}
+        />
+        <mesh
+          name="Cube009_1"
+          geometry={nodes.Cube009_1.geometry}
+          material={materials.PurpleOutline}
+        />
+        <mesh
+          name="Cube009_2"
+          geometry={nodes.Cube009_2.geometry}
+          material={materials.RedCoral}
+        />
+        <mesh
+          name="Cube009_3"
+          geometry={nodes.Cube009_3.geometry}
+          material={materials.Black}
+        />
+        <mesh
+          name="Cube009_4"
+          geometry={nodes.Cube009_4.geometry}
+          material={materials.Kelp}
+        />
+        <mesh
+          name="Cube009_5"
+          geometry={nodes.Cube009_5.geometry}
+          material={materials.KelpOutline}
+        />
+        <mesh
+          name="Cube009_6"
+          geometry={nodes.Cube009_6.geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          name="Cube009_7"
+          geometry={nodes.Cube009_7.geometry}
+          material={materials.SmallCoralOutline}
+        />
+        <mesh
+          name="Cube009_8"
+          geometry={nodes.Cube009_8.geometry}
+          material={materials.KelpOrange}
+        />
+        <mesh
+          name="Cube009_9"
+          geometry={nodes.Cube009_9.geometry}
+          material={materials.KelpOrangeOutline}
+        />
+        <mesh
+          name="Cube009_10"
+          geometry={nodes.Cube009_10.geometry}
+          material={materials.SandOutline}
+        />
+        <mesh
+          name="Cube009_11"
+          geometry={nodes.Cube009_11.geometry}
+          material={materials.YellowSandOutline}
+        />
+        <mesh
+          name="Cube009_12"
+          geometry={nodes.Cube009_12.geometry}
+          material={materials.UrchinBlack}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function TallCoral({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+
+  const TALL_CORALS = [
+    "Cylinder004",
+    "Cylinder005",
+    "Cylinder006",
+    "Cylinder007",
+    "Cylinder008",
+  ];
+
+  // Select a random coral
+  const randomCoral =
+    TALL_CORALS[Math.floor(Math.random() * TALL_CORALS.length)];
+
+  return (
+    <group position={position} rotation={rotation} name="TallCoral">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderTallCoral.geometry}>
+          <meshPhongMaterial opacity={0} transparent />
+        </mesh>
+      </RigidBody>
+      <group>
+        <mesh
+          castShadow
+          geometry={nodes[randomCoral].geometry}
+          material={materials.TallCoralMaterial}
+        />
+        <mesh
+          castShadow
+          geometry={nodes[randomCoral + "_1"].geometry}
+          material={materials.TallCoralOutline}
+        />
+      </group>
+    </group>
+  );
+}
+
+export function SmallCoral({ position, rotation }) {
+  const { nodes, materials } = useGLTF("/environment_colliders.glb");
+
+  const SMALL_CORALS = ["Cylinder010", "Cylinder011"];
+
+  // Select a random coral
+  const randomCoral =
+    SMALL_CORALS[Math.floor(Math.random() * SMALL_CORALS.length)];
+
+  return (
+    <group position={position} rotation={rotation} name="SmallCoral">
+      <RigidBody colliders="trimesh" type="fixed">
+        <mesh geometry={nodes.ColliderSmallCoral.geometry}>
+          <meshPhongMaterial opacity={0} transparent />
+        </mesh>
+      </RigidBody>
+      <group>
+        <mesh
+          castShadow
+          geometry={nodes[randomCoral].geometry}
+          material={materials.SmallCoralMaterial}
+        />
+        <mesh
+          castShadow
+          geometry={nodes[randomCoral + "_1"].geometry}
+          material={materials.SmallCoralOutline}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/environment_colliders.glb");
